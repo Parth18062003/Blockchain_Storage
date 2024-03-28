d7e9acaf069bf7bdf2f8f9a8356dea66c64f7831
Parth Kadam <2021.parth.kadam@ves.ac.in>
2024-03-29 02:00:27 +0530

    upload endpoint

--- server/handlers/uploadHandler.js
+++ server/handlers/uploadHandler.js
@@ -1,6 +1,18 @@
 const uploadHandler = async (req, res) => {
     console.log("Upload handler called");
-    res.send("Upload handler called");
+    try {
+        const ipfsHash = req.body.ipfsHash;
+        const filemetadata = req.body.filemetadata; // No need to parse JSON here
+        console.log("File Metadata:", filemetadata);
+        console.log("Uploaded File:", ipfsHash);
+    
+        // Your further processing logic goes here...
+
+        res.json({ ipfsHash });
+    } catch (error) {
+        console.error("Error handling upload:", error);
+        res.status(500).json({ message: "Internal server error." });
+    }
 }
 
 export default uploadHandler;
